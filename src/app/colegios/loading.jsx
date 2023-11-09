b42284248ce8de7e826fb586a8313f373ff6a86d
agupeiretti <agupeiretti@gmail.com>
2023-11-09 18:03:56 -0300

    cambios recientes carrito

--- src/app/colegios/loading.jsx
+++ src/app/colegios/loading.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const loading = () => {
   return (
-    <div className=" w-full h-screen flex bg-zinc-800 justify-center items-center  ">
+    <div className=" w-full h-full flex bg-zinc-800 justify-center items-center overflow-hidden ">
       <Image
         src={"/logosoloblanco.png"}
         width={100}
--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -9,8 +9,23 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
-    setCart([...cart, item]);
+    const existingProduct = cart.find(
+      (product) => product.slug === item.slug && product.size === item.size
+    );
+
+    if (existingProduct) {
+      setCart((prevCart) => {
+        return prevCart.map((product) =>
+          product.slug === item.slug && product.size === item.size
+            ? { ...product, qty: product.qty + item.qty }
+            : product
+        );
+      });
+    } else {
+      setCart([...cart, item]);
+    }
   };
+
   const removeProduct = (slug, size) => {
     setCart((prevCart) => {
       const updatedCart = prevCart.filter(
