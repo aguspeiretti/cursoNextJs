d5144efc240609574b5c160cd4f83da357c3a666
agupeiretti <agupeiretti@gmail.com>
2023-12-12 11:14:28 -0300

    changes

--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -6,7 +6,7 @@ const CartContext = createContext();
 export const useCartContext = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState();
+  const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
     const existingProduct = cart.find(
