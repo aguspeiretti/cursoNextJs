9941d260f1fc0515c78e85a34cd04303592d5e46
agupeiretti <agupeiretti@gmail.com>
2023-12-13 12:48:19 -0300

    prueba

--- src/app/admin/edit/[slug]/page.jsx
+++ src/app/admin/edit/[slug]/page.jsx
@@ -4,14 +4,17 @@ import React from "react";
 const EditPage = async ({ params }) => {
   const { slug } = params;
 
-  const item = await fetch(`http://localhost:3000/api/routes.product/${slug}`, {
-    cache: "no-store",
-    next: {
-      revalidate: 0,
-    },
-  }).then((r) => r.json());
+  const item = await fetch(
+    `http://${process.env.VERCEL_URL}/api/routes.product/${slug}`,
+    {
+      cache: "no-store",
+      next: {
+        revalidate: 0,
+      },
+    }
+  ).then((r) => r.json());
 
-  return <div className="w-full h-full">{/* <EditForm item={item} /> */}</div>;
+  return <div className="w-full h-full">{<EditForm item={item} />}</div>;
 };
 
 export default EditPage;
