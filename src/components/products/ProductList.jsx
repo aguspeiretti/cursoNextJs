072c6456469eb0d820a9e0e6b3397b27bb1b6edf
Agustin Peiretti <agupeiretti@gmail.com>
2023-12-13 15:14:45 -0300

    url

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,7 +1,22 @@
 import ProductCard from "./ProductCard";
 
 
-const getProducts = async()=>{
+// const getProducts = async()=>{
+//   const items = await fetch(
+//     `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/routes.products/${categoria}`,
+//     {
+//       cache: "no-store",
+//       next: {
+//         tags: ["products"],
+//       },
+//     }
+//   );
+//   return await items.json();
+// }
+
+
+const ProductList = async ({ categoria }) => {
+
   const items = await fetch(
     `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/routes.products/${categoria}`,
     {
@@ -10,18 +25,11 @@ const getProducts = async()=>{
         tags: ["products"],
       },
     }
-  );
-  return await items.json();
-}
-
-
-const ProductList = async ({ categoria }) => {
-
-const items = await getProducts()
+  ).then((r) => r.json());
 
   return (
     <div className="w-full h-full flex flex-wrap justify-center pt-4 ">
-      {items?.map((item) => (
+      {items.map((item) => (
         <ProductCard key={item.slug} item={item} />
       ))}{" "}
     </div>
